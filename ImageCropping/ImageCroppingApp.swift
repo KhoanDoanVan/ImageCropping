2769a908bdfdabe9fff6e9a4bd3c8418fed80898
khoanisdev <doanvankhoan124@gmail.com>
2024-12-01 17:19:39 +0700

    Initial Commit

---
+++ ImageCropping/ImageCroppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCroppingApp.swift
+//  ImageCropping
+//
+//  Created by Đoàn Văn Khoan on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageCroppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
